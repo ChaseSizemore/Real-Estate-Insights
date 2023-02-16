c1c7f71ca5e8d215686e318728476bd692bd3000
ChaseSizemore <ChaseLSizemore@gmail.com>
2023-02-15 22:37:09 -0500

    night prog

---
+++ content.js
@@ -0,0 +1,48 @@
+// let coords = document.querySelector('.mLuXec');
+// document.addEventListener('click', ()=>{
+//     console.log('clicked');
+// })
+
+// let regex = new RegExp('/d/d/.d/d/d/d/d/, /d/d/.d/d/d/d/d/')
+
+// document.addEventListener('click', )
+    
+    
+//     setTimeout((e)=>{
+//         console.log("mouse location:", e.clientX, e.clientY)
+//     }, 1000)
+// });
+
+// function onmousemove(e){
+//     console.log("mouse location:", e.clientX, e.clientY)
+// }
+
+// document.addEventListener('click', function onmousemove(e){
+//     console.log("mouse location:", e.clientX, e.clientY)
+// });
+
+
+// document.addEventListener('DOMContentLoaded', (event) => {
+//     console.log('test');
+
+// });
+
+
+// let clipboard = '';
+// chrome.clipboard.onClipboardDataChanged.addListener(
+//     function() {
+//         chrome.clipboard.readText(function(text) {
+//             clipboard = text;
+//             console.log(clipboard);
+//         });
+//     }
+// );
+
+// navigator.clipboard
+//   .readText()
+//   .then(
+//     (clipText) => (console.log(clipText))
+//   );
+
+
+console.log('test');
